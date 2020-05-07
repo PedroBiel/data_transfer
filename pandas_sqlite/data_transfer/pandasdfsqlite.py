2509639287a70dc19cbed987d43192a0f8497727
Pedro Biel <structural.eng.biel@gmail.com>
2020-05-07 12:56:20 +0200

    Update pandasdfsqlite.py

--- pandas_sqlite/data_transfer/pandasdfsqlite.py
+++ pandas_sqlite/data_transfer/pandasdfsqlite.py
@@ -17,14 +17,14 @@ import sqlite3
 class PandasDFSQLite:
 
     def __init__(self, df, dir_db_name, table):
-        """Transfers data from a pandas DataFrame to a SQLite data base."""
+        """Transfer data from a pandas DataFrame to a SQLite data base."""
         
         self.df = df  # pandas DataFrame to convert.
         self.dir_db_name = dir_db_name  # Directory and name of the database.
         self.table = table  # Table of the database.
 
     def df_to_sql(self):
-        """Writes records stored in a DataFrame to a SQL database."""
+        """Write records stored in a DataFrame to a SQL database."""
 
         conn = sqlite3.connect(self.dir_db_name)
         self.df.to_sql(self.table, conn, if_exists='replace', index=False)
